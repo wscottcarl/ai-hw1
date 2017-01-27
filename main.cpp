bfc5f6c35eb1b7c0b627d21f9d7c914224c61833
Scott <wscottcarl@gmail.com>
2017-01-27 17:02:48 -0600

    laid main FW in comments, added files for board class

---
+++ main.cpp
@@ -0,0 +1,30 @@
+
+
+// should accept cmd line options
+// -t N 	generates N test cases and solutions
+// -i 		asks for input to generate board
+// -r 		generates random board
+// -p 		prints solution output 
+//		** when N exceeds 10, print to results.txt **
+
+int main() {
+
+// if -t option ticked
+// create an Nx10 table
+// for N rows, fillBoard(t[i])
+
+// if -i option ticked, ask for 10 character
+// also prompt user to indicate blank space using 0 int
+// if any input isnt an int that's [0,9], retry (unless 'q', then quit)
+// also, if any input repeated, retry (unless 'q', then quit)
+
+// if -r selected, 
+// create arr[10] and fillBoard(arr)
+
+// -p handled elsewhere
+
+// at this point you should have a board or a selection of boards ready for testing
+// call A*(board)
+
+	return 0;
+}
